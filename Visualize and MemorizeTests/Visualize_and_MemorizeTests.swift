848cba4907da4a72ba334545401d7d22846376d0
Richard Zhang <hmzhang@uwaterloo.ca>
2018-02-11 20:14:01 -0500

    initial commit

---
+++ Visualize and MemorizeTests/Visualize_and_MemorizeTests.swift
@@ -0,0 +1,36 @@
+//
+//  Visualize_and_MemorizeTests.swift
+//  Visualize and MemorizeTests
+//
+//  Created by Richard Zhang on 2018-02-11.
+//  Copyright © 2018 Richard Zhang. All rights reserved.
+//
+
+import XCTest
+@testable import Visualize_and_Memorize
+
+class Visualize_and_MemorizeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
