2d33e1e7473ce7577564b3150b1d969c8aa18536
Richard Zhang <hmzhang@uwaterloo.ca>
2018-03-18 19:10:15 -0400

    added texttospeech functionality to memorization section

--- Visualize and Memorize/VisualizedObjectMoreInfoViewController.swift
+++ Visualize and Memorize/VisualizedObjectMoreInfoViewController.swift
@@ -8,13 +8,17 @@
 
 import Foundation
 import UIKit
+import AVKit
 
-class VisualizedObjectMoreInfoViewController: UIViewController {
+class VisualizedObjectMoreInfoViewController: UIViewController, AVSpeechSynthesizerDelegate {
     
     var visualizedObjectImageView: UIImageView!
     var visualizedObjectInfoLabel: UILabel!
     var visualizedObject: VisualizedObject!
-    
+    var ttsButton: RoundButton!
+
+    let speaker = AVSpeechSynthesizer()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
@@ -30,18 +34,42 @@ class VisualizedObjectMoreInfoViewController: UIViewController {
     }
     
     func setupUI(){
+        speaker.delegate = self
         let backButton = UIBarButtonItem()
+        var ttsButtonFrame: CGRect
         backButton.title = "Back"
         navigationController?.navigationBar.topItem?.backBarButtonItem = backButton
         view.backgroundColor = UIColor.white
-        if #available(iOS 11.0, *), let keyWindow = UIApplication.shared.delegate!.window! {
+        if let keyWindow = UIApplication.shared.delegate!.window! {
             visualizedObjectImageView = UIImageView(frame: CGRect(x: 0, y: (navigationController?.navigationBar.frame.maxY)!, width: view.frame.width, height: view.frame.height - (navigationController?.navigationBar.frame.maxY)! - keyWindow.safeAreaInsets.bottom - 100))
-            visualizedObjectImageView.image = visualizedObject.image
-            visualizedObjectInfoLabel = UILabel(frame: CGRect(x: 0, y: visualizedObjectImageView.frame.maxY, width: view.frame.width, height: 100))
-            visualizedObjectInfoLabel.text = visualizedObject.objectName
-            visualizedObjectInfoLabel.textAlignment = .center
-            view.addSubview(visualizedObjectImageView)
-            view.addSubview(visualizedObjectInfoLabel)
+            ttsButtonFrame = CGRect(x: view.frame.width - 80 , y: view.frame.height - (navigationController?.navigationBar.frame.maxY)! - keyWindow.safeAreaInsets.bottom, width: 70, height: 70 )
+        } else {
+            visualizedObjectImageView = UIImageView(frame: CGRect(x: 0, y: (navigationController?.navigationBar.frame.maxY)!, width: view.frame.width, height: view.frame.height - (navigationController?.navigationBar.frame.maxY)! - 100))
+            ttsButtonFrame = CGRect(x: view.frame.width - 80 , y: view.frame.height - 80, width: 70, height: 70 )
         }
+        ttsButton = RoundButton(withFrame:ttsButtonFrame , andButtonColour: UIColor.red, andImage: UIImage(named: "Volume Mute")!)
+        ttsButton.addTarget(self, action: #selector(onTTSButtonClicked), for: .touchUpInside)
+        visualizedObjectImageView.image = visualizedObject.image
+        visualizedObjectInfoLabel = UILabel(frame: CGRect(x: 0, y: visualizedObjectImageView.frame.maxY, width: view.frame.width, height: 100))
+        visualizedObjectInfoLabel.text = visualizedObject.objectName
+        visualizedObjectInfoLabel.textAlignment = .center
+        view.addSubview(visualizedObjectImageView)
+        view.addSubview(visualizedObjectInfoLabel)
+        view.addSubview(ttsButton)
+    }
+    
+    @objc func onTTSButtonClicked() {
+        ttsButton.setImage(UIImage(named: "Volume Up"), for: UIControlState.normal)
+        speak(withPhrase: visualizedObject.objectName!)
+        
+    }
+    
+    func speak(withPhrase phrase: String){
+        let utterance = AVSpeechUtterance(string: phrase)
+        speaker.speak(utterance)
+    }
+    
+    func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didFinish utterance: AVSpeechUtterance) {
+        ttsButton.setImage(UIImage(named: "Volume Mute"), for: UIControlState.normal)
     }
 }
